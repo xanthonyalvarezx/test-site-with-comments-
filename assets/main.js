4a0ed7667bbe2b3f6a90add9ce030865a7ae8c51
AntMan <xanthonyalvarezx@gmail.com>
2020-06-18 15:44:47 -0400

    added more css and added a new font.

--- assets/main.js
+++ assets/main.js
@@ -1,8 +1,11 @@
 // hero image x
-//photos
-//links to external sites
-//description 
-// comment section
+//photos x
+//links to external sites x
+//description x
+// comment section x
+// add name input for comments
+//make the reply input sit under the comments instead of beside them
+
 
 //--------------------------------------------//
 
@@ -10,18 +13,23 @@ const body = document.querySelector('body')
 const main = document.querySelector('main')
 //Header image and text
 const header = document.createElement('div')
-header.append('FancyCatUpcycling')
+header.id = 'header'
+let titleName = document.createElement('h1')
+titleName.id = 'title'
+titleName.append('FancyCatUpcycling')
 let hero = document.createElement('img')
 hero.src = 'assets/images/fancy hero.jpg'
 hero.id = 'hero'
 body.append(header)
-header.append(hero)
+header.append(hero, titleName)
 // description div
 const descript = document.createElement('div')
+descript.id = 'description'
 body.append(descript)
 descript.append('FancyCatUpcycling is a small buisiness in Feasterville pennsylvania. We build and refurbish old or antique furniture. We also build creative, one of a kind peices out of parts and peices we find on our trasure hunts.')
 // photos
 const images = document.createElement('div')
+images.id = 'images'
 let image1 = document.createElement('img')
 image1.src = 'assets/images/yolk.jpg'
 image1.id = 'image1'
@@ -43,6 +51,11 @@ image4.id = 'image4'
 image4.className = 'image'
 body.append(images)
 images.append(image1, image2, image3, image4)
+// iframe
+const map = document.createElement('iframe')
+map.src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24397.996608409696!2d-75.00614489234435!3d40.14786163140874!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c6ad681c50eda9%3A0xd9244bf7b0603f20!2sFeasterville-Trevose%2C%20PA%2019053!5e0!3m2!1sen!2sus!4v1592500916701!5m2!1sen!2sus" width="200" height="250" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"'
+map.id = 'map'
+body.append(map)
 // comment buttons
 const comment = document.createElement('div')
 comment.id = 'commentDiv'
@@ -59,9 +72,10 @@ comment.append(submit, input)
 body.append(comment)
 //
 submit.addEventListener('click', function(){
-    const textbox =  document.createElement('div')
-    textbox.innerText = input.value
-    comment.append(textbox)
+    const textBox =  document.createElement('div')
+    textBox.innerText = input.value
+    textBox.id = 'textBox1'
+    comment.append(textBox)
     let reply = document.createElement('button')
     reply.append('Reply')
     let replyInput = document.createElement('input')
@@ -69,26 +83,28 @@ submit.addEventListener('click', function(){
     replyInput.placeholder = 'reply ...'
     replyInput.id = 'replyInput'
     replyInput.className = 'input'
-    textbox.append(reply, replyInput)
+    textBox.append(reply, replyInput)
 
     
     reply.addEventListener('click', function(){
         let replyDiv = document.createElement('div')
         replyDiv.innerText = (replyInput.value) 
-        textbox.append(replyDiv)
+        textBox.append(replyDiv)
     })
 })
 
 // links
-
+const linkDiv = document.createElement('div')
+linkDiv.id = 'linkDiv'
 let fb = document.createElement('a')
 fb.href = 'https://www.facebook.com/FancyCatUpcycling'
 fb.target ='_blank'
 fb.append('Check us out on Facebook @ FancyCatUpcycling')
-body.append(fb)
 
 let insta = document.createElement('a')
 insta.href = 'https://www.instagram.com/fancy_cat_upcycling/'
 insta.target ='_blank'
 insta.append('Check us out on Instagram @ FancyCatUpcycling')
-body.append(insta)
+
+linkDiv.append(fb,insta)
+body.append(linkDiv)
